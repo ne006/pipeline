8e67fbeea93fa353567f19067870d46a521cbd6c
ne006 <iatikhomirov@gmail.com>
2019-09-03 15:11:06 +0300

    Initial commit

---
+++ pipe_section_spec.rb
@@ -0,0 +1,45 @@
+require 'rspec'
+require_relative './pipe_section.rb'
+
+RSpec.describe PipeSection do
+	describe "#start" do
+		before(:each) do
+			@source = Queue.new
+			@target = Queue.new
+
+			(1..5).each { |i| @source.push i }
+
+			@section = PipeSection.new(
+				source: @source,
+				target: @target,
+				max_workers: 3
+			) do |i, source, target|
+				i*2
+			end
+		end
+
+		it "should get items from the source queue" do
+			@source.close
+			@section.await
+
+			expect(@source.size).to eql(0)
+		end
+
+		it "should put resulting items in the target queue" do
+			source_size = @source.size
+			@source.close
+			@section.await
+
+			expect(@target.size).to eql(source_size)
+		end
+
+		it "should process items" do
+			@source.close
+			@section.await
+
+			result = (1..5).to_a.map { @target.shift }
+
+			expect(result).to match_array (1..5).map { |i| i*2 }
+		end
+	end
+end
---
+++ pipeline_spec.rb
@@ -0,0 +1,50 @@
+require 'rspec'
+require_relative './pipeline.rb'
+
+
+RSpec.describe Pipeline do
+	describe "#start" do
+		before(:each) do
+			@source = Queue.new
+			@target = Queue.new
+
+			(1..5).each { |i| @source.push i }
+
+			@pipeline = Pipeline.new(
+				source: @source,
+				target: @target,
+				sequence: [
+					{	block: (proc { |e| e*2 })	},
+					{
+						block: (proc { |e| e.pow(2) }),
+						max_workers: 3
+					}
+				]
+			)
+		end
+
+		it "should get items from the source queue" do
+			@source.close
+			@pipeline.await
+
+			expect(@source.size).to eql(0)
+		end
+
+		it "should put resulting items in the target queue" do
+			source_size = @source.size
+			@source.close
+			@pipeline.await
+
+			expect(@target.size).to eql(source_size)
+		end
+
+		it "should process items" do
+			@source.close
+			@pipeline.await
+
+			result = (1..5).to_a.map { @target.shift }
+
+			expect(result).to match_array (1..5).map { |i| i*2 }.map { |i| i.pow(2) }
+		end
+	end
+end
